091f5037a8148f2ec971198cb397573d565f65af
MSimX <153300890+MSimX@users.noreply.github.com>
2023-12-22 00:58:02 +0700

    Update PI_3.cpp

--- PI_3.cpp
+++ PI_3.cpp
@@ -1,21 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "rus");
-	int n_1;
-	cin >> n_1;
-	for (int i = 0; i <= n_1; i++) {
-		cout << i << "  ";
-	}
-	cout << endl << endl;
- 
 	srand(time(0));
-	int n = 1 + rand() % 100;
-	cout << n << endl;
-	for (int j = 0; j <= n; j++) {
-		cout << j << "  ";
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x = 1 + rand() % 100;
+		cout << x << "  ";
 	}
-	cout << endl << endl;
 }
