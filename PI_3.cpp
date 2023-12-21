acf0ca005d8a24c712038908e23baa58a12176cb
MSimX <153300890+MSimX@users.noreply.github.com>
2023-12-22 01:11:24 +0700

    Update PI_3.cpp

--- PI_3.cpp
+++ PI_3.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
+
+int getRandomNumber(int min, int max) {
+	return min + rand() % (max - min + 1);
+}
 
 int main() {
 	srand(time(0));
@@ -10,4 +15,21 @@ int main() {
 		int x = 1 + rand() % 100;
 		cout << x << "  ";
 	}
+
+	cout << endl << endl;
+	
+	int n;
+	cout << "Введите количество случайных чисел: ";
+	cin >> n;
+
+	// Устанавливаем начальное значение для генератора случайных чисел
+	srand(static_cast<unsigned int>(time(0)));
+
+	// Генерируем и выводим 'n' случайных чисел
+	for (int i = 0; i < n; i++) {
+		int randomNumber = getRandomNumber(1, 100); // Генерация чисел в диапазоне от 1 до 100
+		cout << randomNumber << " ";
+	}
+
+	return 0;
 }
